292f28dd3a24f8af10b4ff2353a9f8ff09c6513b
boussarilatif <boussarilatif@gmail.com>
2023-05-15 14:08:02 +0100

    feat: update-controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  
+  @Get()
+  findAll(){
+     return 'all books'
+  }
+}
