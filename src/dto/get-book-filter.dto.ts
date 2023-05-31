502dc010be70ed7f571c22b7142b295abaf25ad5
boussarilatif <boussarilatif@gmail.com>
2023-05-31 13:35:07 +0100

    fix: change the publicationDate to publication_date

--- src/dto/get-book-filter.dto.ts
+++ src/dto/get-book-filter.dto.ts
@@ -3,6 +3,6 @@ import { Language } from "src/books/entities/books.entity";
 export class GetBookFilterDto {
   search?: string;
   author?: string;
-  publication_date?: string; // Expected format: YYYY-MM-DD
+  publication_date?: string;
   language?: Language;
 }
