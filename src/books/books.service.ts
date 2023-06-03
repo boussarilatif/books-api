eb2fde84db7c90182192d34b20a9a30e025132d9
boussarilatif <boussarilatif@gmail.com>
2023-06-03 19:50:56 +0100

    feat: add sample of books

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,14 +1,39 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Book } from './entities/books.entity';
+import { Book, Language } from './entities/books.entity';
 import { CreateBookDto } from 'src/dto/create-book.dto';
 import generateId from 'src/helper/generateId';
 import { UpdateBookDto } from 'src/dto/update-book.dto';
 
 @Injectable()
 export class BooksService {
-  private books: Book[] = []
+  private books: Book[] = [
+    {
+      "id": 1,
+      "title": "The Pragmatic Programmer",
+      "author": "Andrew Hunt, David Thomas",
+      "publicationDate": "1999-10-20",
+      "numberOfPages": 352,
+      "language": Language.ENGLISH
+    },
+    {
+      "id": 2,
+      "title": "Harry Potter and the Philosopher's Stone",
+      "author": "J.K. Rowling",
+      "publicationDate": "1997-06-26",
+      "numberOfPages": 223,
+      "language": Language.ENGLISH
+    },
+    {
+      "id": 3,
+      "title": "Nineteen Eighty-Four",
+      "author": "George Orwell",
+      "publicationDate": "1949-06-08",
+      "numberOfPages": 328,
+      "language": Language.ENGLISH
+    }
+  ]
 
-  findBooks(){
+  findBooks() {
     return this.books
   }
 
@@ -30,7 +55,7 @@ export class BooksService {
     return book;
   }
 
-  updateBook(bookId: number, updatedBook: UpdateBookDto){
+  updateBook(bookId: number, updatedBook: UpdateBookDto) {
     const book = this.findBookById(bookId)
     Object.assign(book, updatedBook);
     return book;
