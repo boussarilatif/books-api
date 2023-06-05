2e8cd65603835bf319e52da98219c5aa0c7923ac
boussarilatif <boussarilatif@gmail.com>
2023-06-05 14:31:53 +0100

    feat: add validation rules

--- src/dto/create-book.dto.ts
+++ src/dto/create-book.dto.ts
@@ -1,12 +1,24 @@
-import { IsString } from 'class-validator';
+import { IsDateString, IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Language } from "src/books/entities/books.entity";
 
 export class CreateBookDto {
   @IsString()
+  @IsNotEmpty()
   title: string;
   
+  @IsString()
+  @IsNotEmpty()
   author: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   publicationDate: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   numberOfPages: number;
+
+  @IsEnum(Language)
+  @IsNotEmpty()
   language: Language;
 }
--- src/dto/get-book-filter.dto.ts
+++ src/dto/get-book-filter.dto.ts
@@ -1,8 +1,16 @@
+import { IsDateString, IsEnum, IsString } from "class-validator";
 import { Language } from "src/books/entities/books.entity";
 
 export class GetBookFilterDto {
+  @IsString()
   search?: string;
+  
+  @IsString()
   author?: string;
+
+  @IsDateString()
   publication_date?: string;
+
+  @IsEnum(Language)
   language?: Language;
 }
