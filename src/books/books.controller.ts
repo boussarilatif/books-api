9b9d0609c2cb37e514b394b286d93d5bb5629734
boussarilatif <boussarilatif@gmail.com>
2023-06-05 02:01:11 +0100

    feat: global pipe

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -24,7 +24,7 @@ export class BooksController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: number, @Body() body: UpdateBookDto) {
+  update(@Param('id') id: string, @Body() body: UpdateBookDto) {
     const book = this.bookService.updateBook(+id, body);
     return book;
   }
