a0c9db7fa3eba50a84d97001126773d8b386263a
boussarilatif <boussarilatif@gmail.com>
2023-06-13 00:53:10 +0100

    feat: setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './books/entities/books.entity';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'book_database',
+      entities: [Book],
+      synchronize: true,
+    }),
+    BooksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/books/entities/books.entity.ts
+++ src/books/entities/books.entity.ts
@@ -1,13 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
 export enum Language {
   ENGLISH = 'English',
   FRENCH = 'French',
 }
 
-export interface Book {
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({nullable: true})
   title: string;
+
+  @Column({ nullable: true })
   author: string;
+
+  @Column({ type: 'date',nullable: true })
   publicationDate: string;
+
+  @Column({nullable: true})
   numberOfPages: number;
+
+  @Column({ type: 'enum', enum: Language,nullable: true })
   language: Language;
 }
